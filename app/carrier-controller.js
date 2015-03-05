392f633a7d0107a7403a6c0a22fa53c5f0fcfd21
Frank E Weiss <feweiss@gmail.com>
2015-03-04 20:02:59 -0800

    Remove dead code,

--- app/carrier-controller.js
+++ app/carrier-controller.js
@@ -1,8 +1,4 @@
 angular.module('carrier', [ 'rides', 'agencies' ])
-// this is deprecated in 1.2 and removed in 1.3
-.config(function ($parseProvider) {
-    $parseProvider.unwrapPromises(true);
-})
 .controller('Trip', [ '$scope', 'chart', 'sfMuni', function($scope, chart, SfMuni) {
     $scope.disableOrigin = true;
     $scope.disableDestination = true;
@@ -44,20 +40,4 @@ angular.module('carrier', [ 'rides', 'agencies' ])
             });
         }
    }
-    function getPlan() {
-        var plan =  { spanStart: new Date('2013-02-22T13:00'), spanEnd: new Date('2013-02-22T14:00'),
-            rides: [
-            { agency: 'sfmuni', route: '55', origin: '3rd', destination: '16th', rides: [
-                [ '20150221T1300', '20150222T1315' ]
-            ],
-                rideStart: new Date('2013-02-22T13:10'), rideEnd: new Date('2013-02-22T13:20')
-            }
-        ]};
-        return plan;
-    }
-}])
-.factory('stops', [ '$resource', function($resource) {
-    return $resource('http://webservices.nextbus.com/service/publicXMLFeed', { command: 'routeList', a: 'sf-muni' }, {
-        query: {method:'GET', params:{}, isArray:true}
-    });
 }]);
