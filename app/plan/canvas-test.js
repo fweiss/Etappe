a18eabd6141b1876de06e449ad87defd2695156f
Frank E Weiss <feweiss@gmail.com>
2015-03-10 19:09:08 -0700

    Add 5 and 15 minute ticks.

--- app/plan/canvas-test.js
+++ app/plan/canvas-test.js
@@ -8,52 +8,84 @@ describe('canvas5', function() {
         return new Date(date.getTime() + minutes * 60000);
     }
     beforeEach(module('rides'));
-    describe('directive', function() {
-        var element;
-        var scope;
-        var mockContext = jasmine.createSpyObj('mockContext', [ 'save', 'restore', 'fillRect', 'beginPath', 'moveTo', 'lineTo', 'stroke' ]);
-        beforeEach(inject(function($rootScope, $compile, chart) {
-            scope = $rootScope;
-            var html = '<canvas utt-rides width="600" height="100"></canvas>';
-            element = angular.element(html);
-            // angular.element() evidently ignores style attribute
-            element[0].width = canvasWidth;
-            element[0].height = canvasHeight;
-            $compile(element)(scope);
-            chart.setTimeSpan(spanStart, spanEnd);
-            element[0].getContext = function() {
-                return mockContext;
-            };
-        }));
-        it('should draw background', function() {
-            scope.plan = { spanStart: spanStart, spanEnd: spanEnd, rides: [  { startTime: spanStart, endTime: spanEnd }]};
-            element.scope().$apply();
-            expect(mockContext.fillRect).toHaveBeenCalledWith(0, 0, canvasWidth, canvasHeight);
+    var element;
+    var scope;
+    var mockContext;
+    function setPlanAndApply(plan) {
+        scope.plan = plan;
+        element.scope().$apply();
+    }
+    beforeEach(inject(function($rootScope, $compile, chart) {
+        scope = $rootScope;
+        var html = '<canvas utt-rides width="600" height="100"></canvas>';
+        element = angular.element(html);
+        // angular.element() evidently ignores style attribute
+        element[0].width = canvasWidth;
+        element[0].height = canvasHeight;
+        $compile(element)(scope);
+        //chart.setTimeSpan(spanStart, spanEnd);
+
+        mockContext = jasmine.createSpyObj('mockContext', [ 'save', 'restore', 'fillRect', 'beginPath', 'moveTo', 'lineTo', 'stroke', 'fillText' ]);
+        element[0].getContext = function() {
+            return mockContext;
+        };
+    }));
+    it('should draw background', function() {
+        scope.plan = { spanStart: spanStart, spanEnd: spanEnd, rides: [  { startTime: spanStart, endTime: spanEnd }]};
+        element.scope().$apply();
+        expect(mockContext.fillRect).toHaveBeenCalledWith(0, 0, canvasWidth, canvasHeight);
+    });
+    it('should draw a ride', function() {
+        var rideStart = addMinutes(spanStart, 10);
+        var rideEnd = addMinutes(spanStart, 20);
+        scope.plan = { spanStart: spanStart, spanEnd: spanEnd, rides: [ { startTime: rideStart, endTime: rideEnd }]};
+        element.scope().$apply();
+        expect(mockContext.moveTo).toHaveBeenCalledWith(100, 0);
+        expect(mockContext.lineTo).toHaveBeenCalledWith(200, canvasHeight);
+    });
+    it('should draw two rides', function() {
+        var rideStart0 = addMinutes(spanStart, 10);
+        var rideEnd0 = addMinutes(spanStart, 20);
+        var rideStart1 = addMinutes(spanStart, 30);
+        var rideEnd1 = addMinutes(spanStart, 43);
+        var plan = { spanStart: spanStart, spanEnd: spanEnd,
+            rides: [
+                { startTime: rideStart0, endTime: rideEnd0 },
+                { startTime: rideStart1, endTime: rideEnd1 }
+            ]
+        };
+        scope.plan = plan;
+        element.scope().$apply();
+        expect(mockContext.moveTo).toHaveBeenCalledWith(100, 0);
+        expect(mockContext.lineTo).toHaveBeenCalledWith(200, canvasHeight);
+        expect(mockContext.moveTo).toHaveBeenCalledWith(300, 0);
+    });
+    describe('time ticks', function() {
+        it('should draw a tick', function() {
+            var plan = { spanStart: addMinutes(spanStart, 1), spanEnd: addMinutes(spanStart, 6) };
+            setPlanAndApply(plan);
+            // 5 minute span 600 px 4/5 of 600 = 480
+            var x = 4 / 5 * 600;
+            expect(mockContext.beginPath).toHaveBeenCalled();
+            expect(mockContext.moveTo).toHaveBeenCalledWith(x, 0);
+            expect(mockContext.lineTo).toHaveBeenCalledWith(x, canvasHeight);
+            expect(mockContext.stroke).toHaveBeenCalled();
         });
-        it('should draw a ride', function() {
-            var rideStart = addMinutes(spanStart, 10);
-            var rideEnd = addMinutes(spanStart, 20);
-            scope.plan = { spanStart: spanStart, spanEnd: spanEnd, rides: [ { startTime: rideStart, endTime: rideEnd }]};
-            element.scope().$apply();
-            expect(mockContext.moveTo).toHaveBeenCalledWith(100, 0);
-            expect(mockContext.lineTo).toHaveBeenCalledWith(200, canvasHeight);
+        it('should draw all ticks for span', function() {
+            var plan = { spanStart: spanStart, spanEnd: addMinutes(spanStart, 60) };
+            setPlanAndApply(plan);
+            expect(mockContext.moveTo.calls.count()).toBe(12);
         });
-        it('should draw two rides', function() {
-            var rideStart0 = addMinutes(spanStart, 10);
-            var rideEnd0 = addMinutes(spanStart, 20);
-            var rideStart1 = addMinutes(spanStart, 30);
-            var rideEnd1 = addMinutes(spanStart, 43);
-            var plan = { spanStart: spanStart, spanEnd: spanEnd,
-                rides: [
-                    { startTime: rideStart0, endTime: rideEnd0 },
-                    { startTime: rideStart1, endTime: rideEnd1 }
-                ]
-            };
-            scope.plan = plan;
-            element.scope().$apply();
-            expect(mockContext.moveTo).toHaveBeenCalledWith(100, 0);
-            expect(mockContext.lineTo).toHaveBeenCalledWith(200, canvasHeight);
-            expect(mockContext.moveTo).toHaveBeenCalledWith(300, 0);
+        // can't spy on strokeStyle property
+        it('should draw minor tick', function() {
+            var plan = { spanStart: addMinutes(spanStart, 1), spanEnd: addMinutes(spanStart, 6) };
+            setPlanAndApply(plan);
+            expect(mockContext.strokeStyle).toBe('rgba(0, 0, 0, 0.1)');
+        });
+        it('should draw major tick', function() {
+            var plan = { spanStart: addMinutes(spanStart, 12), spanEnd: addMinutes(spanStart, 16) };
+            setPlanAndApply(plan);
+            expect(mockContext.strokeStyle).toBe('rgba(0, 0, 0, 0.5)');
         });
     });
 });
--- app/plan/canvas.js
+++ app/plan/canvas.js
@@ -1,39 +1,94 @@
 angular.module('rides')
-.directive('uttRides', function(chart) {
-    var width;
-    var height;
-    function clearCanvas(canvas) {
-        canvas.width = canvas.width;
-    }
-    return {
-        restrict: 'A',
-        link: function(scope, element) {
-            width = element[0].width;
-            height = element[0].height;
-            chart.setWidth(width);
-            scope.$watch('plan', function(plan) {
-                if (plan) {
-                    chart.setTimeSpan(plan.spanStart, plan.spanEnd);
-                    clearCanvas(element[0]);
-                    var ctx = element[0].getContext('2d');
-                    ctx.save();
+    .directive('uttRides', function(chart) {
+        var rideLine = { lineWidth: 6, strokeStyle: 'rgba(255, 255, 255, 1' };
+        var timeTickMajor = { lineWidth: 1, strokeStyle: 'rgba(0, 0, 0, 0.5)' };
+        var timeTickMinor = { lineWidth: 1, strokeStyle: 'rgba(0, 0, 0, 0.1)' };
+        var width;
+        var height;
 
-                    ctx.fillStyle = '#7f7fff';
-                    ctx.fillRect(0, 0, width, height);
-                    _.each(plan.rides, function(ride) {
-                        var startTime = ride.startTime;
-                        var endTime = ride.endTime;
+        var hourMillis = 60 * 60 * 1000;
+        var minuteMillis = 60 * 1000;
 
-                        ctx.strokeStyle = "rgba(0, 0, 0, 1)";
-                        ctx.lineWidth = 10;
-                        ctx.beginPath();
-                        ctx.moveTo(chart.timeToX(startTime), 0);
-                        ctx.lineTo(chart.timeToX(endTime), height);
-                        ctx.stroke();
-                    });
-                    ctx.restore();
-                }
-            });
+        var timeToX = chart.timeToX;
+
+        function addMinutes(date, minutes) {
+            return new Date(date.getTime() + minutes * 60000);
         }
-    };
-});
+        function clearCanvas(canvas) {
+            canvas.width = canvas.width;
+        }
+        return {
+            restrict: 'A',
+            link: function(scope, element) {
+                width = element[0].width;
+                height = element[0].height;
+                chart.setWidth(width);
+                scope.$watch('plan', function(plan) {
+                    if (plan) {
+                        chart.setTimeSpan(plan.spanStart, plan.spanEnd);
+                        clearCanvas(element[0]);
+                        var ctx = element[0].getContext('2d');
+                        ctx.save();
+
+                        ctx.fillStyle = '#7f7fff';
+                        ctx.fillRect(0, 0, width, height);
+                        drawTimeTickMajor(ctx, plan.spanStart, plan.spanEnd);
+
+                        _.each(plan.rides, function(ride) {
+                            var startTime = ride.startTime;
+                            var endTime = ride.endTime;
+
+                            ctx.strokeStyle = rideLine.strokeStyle;
+                            ctx.lineWidth = rideLine.lineWidth;
+                            ctx.beginPath();
+                            ctx.moveTo(chart.timeToX(startTime), 0);
+                            ctx.lineTo(chart.timeToX(endTime), height);
+                            ctx.stroke();
+                        });
+                        ctx.restore();
+                    }
+                });
+            }
+        };
+        function drawTimeTickMajor(ctx, spanStart, spanEnd) {
+            var fiveMinuteMillis = 5 * minuteMillis;
+            var fifteenMinutesMillis = 15 * minuteMillis;
+            var t0 = Math.ceil(spanStart.getTime() / fiveMinuteMillis) * fiveMinuteMillis;
+            var t1 = spanEnd.getTime();
+            var fifteenMinuteTimes = [];
+            var fiveMinuteTimes = [];
+            for (var t=t0; t<t1; t+=fiveMinuteMillis) {
+                var majorTick = t % fifteenMinutesMillis == 0;
+                if (majorTick) {
+                    fifteenMinuteTimes.push(new Date(t));
+                } else {
+                    fiveMinuteTimes.push(new Date(t));
+                }
+            }
+            ctx.lineWidth = timeTickMajor.lineWidth;
+            _.each(fifteenMinuteTimes, function(time) {
+                ctx.strokeStyle = timeTickMajor.strokeStyle;
+                var x = timeToX(time);
+                drawTick(x);
+            });
+            ctx.lineWidth = timeTickMinor.lineWidth;
+            _.each(fiveMinuteTimes, function(time) {
+                ctx.strokeStyle = timeTickMinor.strokeStyle;
+                var x = timeToX(time);
+                drawTick(x);
+            });
+            function drawTick(x) {
+                ctx.beginPath();
+                ctx.moveTo(x, 0);
+                ctx.lineTo(x, height);
+                ctx.stroke();
+            }
+        }
+        /** Time of Day */
+        function tod(time) {
+            // 3:34:00 PM - 3:34 PM
+            var tt =  time.toLocaleTimeString();
+            var p1 = tt.indexOf(" ");
+            return tt.substring(0, p1 - 3) + tt.substring(p1);
+        }
+    });
