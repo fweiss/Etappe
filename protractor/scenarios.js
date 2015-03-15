65b38553c00089cfe6549a698c1b07d58941f507
Frank E Weiss <feweiss@gmail.com>
2015-03-14 17:33:33 -0700

    Incorporate mountebank fixture into Protractor scenario.

--- protractor/scenarios.js
+++ protractor/scenarios.js
@@ -6,6 +6,8 @@ describe('carrier select', function() {
             angular.module('sfmuni.config', {})
                 .value('config', { baseUrl: 'http://localhost:4545' });
         });
+        var flow = protractor.promise.controlFlow();
+        flow.execute(mountebank);
         browser.get('http://localhost:8080/app/index.html');
     });
     xit('should prompt to select carrier', function() {
@@ -53,5 +55,107 @@ describe('carrier select', function() {
             expect(element(by.binding('rideList')).getText()).toBeGreaterThan(0);
         })
     });
+    function mountebank() {
+        var request = require('request');
+        var defer = protractor.promise.defer();
+        var options = {};
+        options.url = 'http://localhost:2525/imposters';
+        options.method = 'PUT';
+        options.headers = { 'Content-type': 'application/json' };
+        options.json = true;
+        var headers = {
+            'Content-type': 'application/json',
+            'Access-Control-Allow-Origin': '*'
+        };
+        options.body = {
+            imposters: [
+                {
+                    port: 4545,
+                    protocol: 'http',
+                    stubs: [
+                        {
+                            responses: [
+                                {
+                                    is: {
+                                        status: 200,
+                                        headers: headers,
+                                        body: '<body>'
+                                            + '<route tag="N">'
+                                            + '<stop tag="5555" title="16th St and Mission" stopId="15555"></stop>'
+                                            + '<stop tag="4444" title="16th St and Harrison" stopId="14444"></stop>'
+                                            + '</route>'
+                                            + '</body>'
+                                    }
+                                }
+                            ],
+                            predicates: [
+                                {
+                                    equals: {
+                                        query: {
+                                            command: 'routeConfig'
+                                        }
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            responses: [
+                                {
+                                    is: {
+                                        status: 200,
+                                        headers: headers,
+                                        body: '<body><predictions routeTag="N"><direction><prediction epochTime="2222" vehicle="3333" tripTag="7777"></prediction></direction></predictions></body>'
+                                    }
+                                }
+                            ],
+                            predicates: [
+                                {
+                                    equals: {
+                                        query: {
+                                            command: 'predictionsForMultiStops',
+                                            stops: 'N|4444'
+                                        }
+                                    }
+                                }
+                            ]
+                        },
+                        {
+                            responses: [
+                                {
+                                    is: {
+                                        status: 200,
+                                        headers: headers,
+                                        body: '<body><predictions routeTag="N"><direction><prediction epochTime="1111" vehicle="3333" tripTag="7777"></prediction></direction></predictions></body>'
+                                    }
+                                }
+                            ],
+                            predicates: [
+                                {
+                                    equals: {
+                                        query: {
+                                            command: 'predictionsForMultiStops',
+                                            stops: 'N|5555'
+                                        }
+                                    }
+                                }
+                            ]
+                        }
+
+                    ]
+                }
+            ]
+        };
+        request.put(options, function(error, message, body) {
+            if (error || message.statusCode >= 400) {
+                defer.reject({
+                    error : error,
+                    message : message
+                });
+            } else {
+                defer.fulfill(message);
+            }
+        });
+        return defer.promise;
+    }
 });
 
