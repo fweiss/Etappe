e72f2b4503be3158dcf1fd465cd7691cbc13bfa6
Frank E Weiss <feweiss@gmail.com>
2016-09-17 11:42:32 -0700

    Refactor: itinerary use waypoint instead of nexus

--- app/modules/plan/itinerary-test.js
+++ app/modules/plan/itinerary-test.js
@@ -3,19 +3,19 @@ describe('domain itinerary', function() {
     var Trip;
     var Waypoint;
     beforeEach(module('plan'));
-    beforeEach(inject(function (_itinerary_, _trip_, _nexus_) {
+    beforeEach(inject(function (_itinerary_, _trip_, _waypoint_) {
         Itinerary = _itinerary_;
         Trip = _trip_;
-        Waypoint = _nexus_;
+        Waypoint = _waypoint_;
     }));
     var trip;
     var w1;
     var w2;
     var w3;
     beforeEach(function() {
-        w1 = Waypoint.create('w1', 21, 31);
-        w2 = Waypoint.create('w2', 22, 32);
-        w2 = Waypoint.create('w3', 23, 33);
+        w1 = Waypoint.createWaypoint('w1', 21, 31);
+        w2 = Waypoint.createWaypoint('w2', 22, 32);
+        w2 = Waypoint.createWaypoint('w3', 23, 33);
         trip = Trip.createTrip(w1, w2);
    });
     describe('creation', function() {
