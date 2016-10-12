b96db092bb1e582b8615891acb2930c5e6d83f70
Frank E Weiss <feweiss@gmail.com>
2016-10-12 08:15:03 -0700

    Refactor Itinerary to type.

--- app/modules/plan/itinerary.js
+++ app/modules/plan/itinerary.js
@@ -1,28 +1,31 @@
 angular.module('plan')
-    .service('itinerary', [ function() {
-        return {
-            createItinerary: function(_trip, _segments) {
-                if (_.isUndefined(_trip)) {
-                    throw new Error('createItinerary: trip is required');
-                }
-                var trip = _trip;
-                var segments = [];
-                if (_segments) {
-                    segments = _segments;
-                } else {
-                    _.reduce(_.union([ trip.getOrigin() ], trip.getInnerWaypoints(), [ trip.getDestination() ]), function(previousWaypoint, waypoint) {
-                        segments.push({ originNexus: previousWaypoint, destinationNexus: waypoint, rides: []});
-                        return waypoint;
-                    });
-                }
-                return {
-                    getTrip: function() {
-                        return trip;
-                    },
-                    getSegments: function() {
-                        return segments;
-                    }
-                };
+.service('itinerary', [ function() {
+    function Itinerary(trip, segments) {
+        this.trip = trip;
+        this.segments = segments;
+    };
+    Itinerary.prototype.getTrip = function() {
+        return this.trip;
+    };
+    Itinerary.prototype.getSegments = function() {
+        return this.segments;
+    };
+    return {
+        createItinerary: function(_trip, _segments) {
+            if (_.isUndefined(_trip)) {
+                throw new Error('createItinerary: trip is required');
             }
+            var trip = _trip;
+            var segments = [];
+            if (_segments) {
+                segments = _segments;
+            } else {
+                _.reduce(_.union([ trip.getOrigin() ], trip.getInnerWaypoints(), [ trip.getDestination() ]), function(previousWaypoint, waypoint) {
+                    segments.push({ originNexus: previousWaypoint, destinationNexus: waypoint, rides: []});
+                    return waypoint;
+                });
+            }
+            return new Itinerary(trip, segments);
         }
-    }]);
+    }
+}]);
