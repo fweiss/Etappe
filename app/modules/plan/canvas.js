3282b5aefd116e8ee69089d80992f5c1d1d08d7a
Frank E Weiss <feweiss@gmail.com>
2016-10-16 18:34:58 -0700

    Refactor: move directive return to top, extract drawElement()

--- app/modules/plan/canvas.js
+++ app/modules/plan/canvas.js
@@ -1,3 +1,4 @@
+'use strict';
 angular.module('plan')
     .directive('uttRides', function(chart, planConfig, date) {
         //console.log('mmmmmmmm ' + tickLegendHeight);
@@ -13,56 +14,53 @@ angular.module('plan')
 
         var timeToX = chart.timeToX;
 
-        function addMinutes(date, minutes) {
-            return new Date(date.getTime() + minutes * 60000);
-        }
-        function clearCanvas(canvas) {
-            canvas.width = canvas.width;
-        }
         return {
             restrict: 'A',
             link: function(scope, element) {
                 width = element[0].width;
                 height = element[0].height;
                 chart.setWidth(width);
-                //scope.$watch('plan', function(plan) {
-                scope.$watch('itinerary.getSpan()', function(newValue, oldValue, scope) {
-                    var itinerary = scope.itinerary;
-                    if (itinerary) {
-                        var span = itinerary.getSpan();
-                        var segment = itinerary.getSegments() && itinerary.getSegments()[0];
-                        var rides = (segment && segment.rides) || [];
-
-                        chart.setTimeSpan(span.spanStart, span.spanEnd);
-                        clearCanvas(element[0]);
-                        var ctx = element[0].getContext('2d');
-                        ctx.save();
-
-                        ctx.fillStyle = '#7f7fff';
-                        ctx.fillRect(0, tickLegendHeight, width, height);
-                        drawTimeTickMajor(ctx, span.spanStart, span.spanEnd);
-
-                        _.each(rides, function(ride) {
-                            var startTime = ride.startTime;
-                            var endTime = ride.endTime;
-
-                            ctx.strokeStyle = rideLine.strokeStyle;
-                            ctx.lineWidth = rideLine.lineWidth;
-                            ctx.lineCap = 'square';
-                            ctx.beginPath();
-                            ctx.moveTo(chart.timeToX(startTime), 0);
-                            ctx.lineTo(chart.timeToX(endTime), height);
-                            ctx.stroke();
-                        });
-                        if (segment) {
-                            ctx.font = 'bold 12pt Calibri';
-                            ctx.fillText(segment.getOriginNexus().waypoint.name, 0, tickLegendHeight);
-                        }
-                        ctx.restore();
-                    }
-                }, true);
+                scope.$watch('itinerary.getSpan()', watchListener, true);
+                function watchListener(newValue, oldValue, scope) {
+                    drawElement(element, scope);
+                }
             }
         };
+        function drawElement(element, scope) {
+            var itinerary = scope.itinerary;
+            if (itinerary) {
+                var span = itinerary.getSpan();
+                var segment = itinerary.getSegments() && itinerary.getSegments()[0];
+                var rides = (segment && segment.rides) || [];
+
+                chart.setTimeSpan(span.spanStart, span.spanEnd);
+                clearCanvas(element[0]);
+                var ctx = element[0].getContext('2d');
+                ctx.save();
+
+                ctx.fillStyle = '#7f7fff';
+                ctx.fillRect(0, tickLegendHeight, width, height);
+                drawTimeTickMajor(ctx, span.spanStart, span.spanEnd);
+
+                _.each(rides, function(ride) {
+                    var startTime = ride.startTime;
+                    var endTime = ride.endTime;
+
+                    ctx.strokeStyle = rideLine.strokeStyle;
+                    ctx.lineWidth = rideLine.lineWidth;
+                    ctx.lineCap = 'square';
+                    ctx.beginPath();
+                    ctx.moveTo(chart.timeToX(startTime), 0);
+                    ctx.lineTo(chart.timeToX(endTime), height);
+                    ctx.stroke();
+                });
+                if (segment) {
+                    ctx.font = 'bold 12pt Calibri';
+                    ctx.fillText(segment.getOriginNexus().waypoint.name, 0, tickLegendHeight);
+                }
+                ctx.restore();
+            }
+        }
         function drawTimeTickMajor(ctx, spanStart, spanEnd) {
             var fiveMinuteMillis = 5 * minuteMillis;
             var fifteenMinutesMillis = 15 * minuteMillis;
@@ -111,5 +109,11 @@ angular.module('plan')
             // 3:34:00 PM - 3:34 PM
             var tt =  time.toLocaleTimeString();
             return date.format(tt);
+        }
+        function addMinutes(date, minutes) {
+            return new Date(date.getTime() + minutes * 60000);
+        }
+        function clearCanvas(canvas) {
+            canvas.width = canvas.width;
         }
     });
