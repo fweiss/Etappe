ee4d50eca65435fef242cd2c5e7c12961bd597c0
Frank E Weiss <feweiss@gmail.com>
2016-12-11 12:04:48 -0800

    Postfactor: chart tests, remove dead code

--- app/modules/plan/canvas-test.js
+++ app/modules/plan/canvas-test.js
@@ -1,4 +1,4 @@
-fdescribe('itinerary chart', function() {
+describe('itinerary chart', function() {
     // N.B. Date constructor for ISO 8601 is always UTC, use RFC2822 instead
     //var spanStart = new Date('2013-02-22T13:00');
     var spanStart = new Date('22 Feb 2013 13:00');
@@ -89,10 +89,6 @@ fdescribe('itinerary chart', function() {
         w3 = Waypoint.createWaypoint('w3', 1, 4);
 
     });
-
-
-
-
 
     describe('canvas properties', function() {
         it('noop on empty span', function() {
@@ -247,148 +243,6 @@ fdescribe('itinerary chart', function() {
         });
     });
 
-
-
-
-
-    describe('600 x 100', function() {
-        // TODO error conditions
-        //var itinerary;
-        var tickLegendHeight;
-
-        beforeEach(function() {
-            mockCanvasContext(canvasWidth, canvasHeight);
-            tickLegendHeight = planConfig('tickLegendHeight');
-            canvasHeight = config.tickLegendHeight + 2 * config.waypointLegendHeight + 1 * config.pathFieldHeight;
-        });
-        //it('noop on empty span', function() {
-        //    var trip1 = Trip.createTrip(Waypoint.createWaypoint('w1', 1, 2), Waypoint.createWaypoint('w2', 1, 2));
-        //    var itineraryNoSpan = Itinerary.createItinerary(trip1);
-        //    scope.itinerary = itineraryNoSpan;
-        //    expect(function() { element.scope().$apply(); }).not.toThrow();
-        //    expect(mockContext.fillRect).not.toHaveBeenCalled();
-        //});
-        //it('should draw background', function() {
-        //    setItineraryAndApply(itinerary);
-        //    expect(mockContext.fillRect).toHaveBeenCalledWith(0, config.tickLegendHeight, canvasWidth, config.pathFieldHeight);
-        //});
-        //it('should draw a ride', function() {
-        //    var segment = itinerary.getSegments()[0];
-        //    segment.setRides([ { startTime: addMinutes(spanStart, 1), endTime: addMinutes(spanStart, 2) } ]);
-        //    setItineraryAndApply(itinerary);
-        //    expect(mockContext.moveTo).toHaveBeenCalledWith(10, config.tickLegendHeight);
-        //    expect(mockContext.lineTo).toHaveBeenCalledWith(20, canvasHeight);
-        //});
-        //it('should draw two rides', function() {
-        //    var rideStart0 = addMinutes(spanStart, 11);
-        //    var rideEnd0 = addMinutes(spanStart, 21);
-        //    var rideStart1 = addMinutes(spanStart, 31);
-        //    var rideEnd1 = addMinutes(spanStart, 43);
-        //    var rides = itinerary.getSegments()[0].getRides();
-        //    rides.push(createRide(rideStart0, rideEnd0));
-        //    rides.push(createRide(rideStart1, rideEnd1));
-        //    setItineraryAndApply(itinerary);
-        //    expect(mockContext.moveTo).toHaveBeenCalledWith(110, config.tickLegendHeight);
-        //    expect(mockContext.lineTo).toHaveBeenCalledWith(210, canvasHeight);
-        //    expect(mockContext.moveTo).toHaveBeenCalledWith(310, config.tickLegendHeight);
-        //});
-        //describe('time ticks', function() {
-        //    it('should draw a tick', function() {
-        //        itinerary.setSpan(addMinutes(spanStart, 1), addMinutes(spanStart, 6));
-        //        setItineraryAndApply(itinerary);
-        //        // 5 minute span 600 px 4/5 of 600 = 480
-        //        var x = 4 / 5 * 600;
-        //        expect(mockContext.beginPath).toHaveBeenCalled();
-        //        expect(mockContext.moveTo).toHaveBeenCalledWith(x, 0);
-        //        expect(mockContext.lineTo).toHaveBeenCalledWith(x, canvasHeight);
-        //        expect(mockContext.stroke).toHaveBeenCalled();
-        //    });
-        //    it('should draw all ticks for span', function() {
-        //        itinerary.setSpan(spanStart, addMinutes(spanStart, 60));
-        //        setItineraryAndApply(itinerary);
-        //        expect(mockContext.moveTo.calls.count()).toBe(12);
-        //    });
-        //    // can't spy on strokeStyle property
-        //    it('should draw minor tick', function() {
-        //        itinerary.setSpan(addMinutes(spanStart, 1), addMinutes(spanStart, 6));
-        //        setItineraryAndApply(itinerary);
-        //        expect(mockContext.strokeStyle).toBe('rgba(0, 0, 0, 0.1)');
-        //    });
-        //    it('should draw major tick', function() {
-        //        itinerary.setSpan(addMinutes(spanStart, 12), addMinutes(spanStart, 16));
-        //        setItineraryAndApply(itinerary);
-        //        expect(mockContext.strokeStyle).toBe('rgba(0, 0, 0, 0.5)');
-        //    });
-        //});
-        xdescribe('tick labels', function() {
-            it('should draw at 15 minute mark', function() {
-                itinerary.setSpan(addMinutes(spanStart, 12), addMinutes(spanStart, 16));
-                setItineraryAndApply(itinerary);
-                expect(mockContext.fillText).toHaveBeenCalledWith('1:15 PM', 452, 10);
-            });
-            //it('should draw over ride', function() {
-            //    mockContext.fillText.and.callFake(function() {
-            //        expect(mockContext.moveTo).not.toHaveBeenCalled();
-            //    });
-            //    setItineraryAndApply(itinerary);
-            //});
-        });
-        //describe('nexus', function() {
-        //    it('should draw top label', function() {
-        //        setItineraryAndApply(itinerary);
-        //        // FIXME actual position
-        //        var tickLegendHeight = planConfig('tickLegendHeight');
-        //        expect(mockContext.fillText).toHaveBeenCalledWith('w1', 0, config.tickLegendHeight);
-        //        expect(mockContext.font).toBe('bold 12pt Calibri');
-        //    });
-        //});
-        xdescribe('itnerary', function() {
-            var w1, w2, w3;
-            var trip;
-            var canvasHeight;
-            beforeEach(function() {
-                w1 = Waypoint.createWaypoint('n1', 1, 2);
-                w2 = Waypoint.createWaypoint('n2', 1, 3);
-                w3 = Waypoint.createWaypoint('n3', 1, 4);
-            });
-            //describe('with 1 segment', function() {
-            //    beforeEach(function() {
-            //        var itinerary = createItineraryForWaypoints([ w1, w2 ]);
-            //        itinerary.setSpan(new Date(1), new Date(2));
-            //        setItineraryAndApply(itinerary);
-            //        canvasHeight = config.tickLegendHeight + 2 * config.waypointLegendHeight + 1 * config.pathFieldHeight;
-            //    });
-            //    it('should have total height', function() {
-            //        expect(element[0].height).toEqual(canvasHeight);
-            //    });
-            //});
-            describe('with 2 segments', function() {
-                beforeEach(function() {
-                    var itinerary = createItineraryForWaypoints([w1, w2, w3]);
-                    itinerary.setSpan(spanStart, addMinutes(spanStart, 1));
-                    setItineraryAndApply(itinerary);
-                    canvasHeight = config.tickLegendHeight + 3 * config.waypointLegendHeight + 2 * config.pathFieldHeight;
-                });
-                it('should have total height', function() {
-                    expect(element[0].height).toEqual(canvasHeight);
-                });
-                it('should draw ticks', function() {
-                    expect(mockContext.lineTo).toHaveBeenCalledWith(0, canvasHeight);
-                });
-                it('should draw two fields', function() {
-                    expect(mockContext.fillRect.calls.count()).toEqual(2);
-                });
-                it('should draw offset fields', function() {
-                    expect(mockContext.translate).toHaveBeenCalledWith(0, 200);
-                    expect(mockContext.fillRect).toHaveBeenCalledWith(0, config.tickLegendHeight, canvasWidth, config.pathFieldHeight);
-                });
-                it('should translate for ticks', function() {
-                    //console.log(mockContext.translate.calls.all()[0]);
-                    expect(mockContext.translate.calls.mostRecent().args).toEqual([ 0, 0 ]);
-                });
-            });
-        });
-    });
     xdescribe('1200 x 200', function() {
         it('should have ticks for entire length', function() {
             mockCanvasContext(1200, 200);
