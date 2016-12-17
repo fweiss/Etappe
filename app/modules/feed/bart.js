4332182a0cf02744d0555d19506f3a2e4d28c512
Frank E Weiss <feweiss@gmail.com>
2016-12-17 08:22:22 -0800

    Postfactor: remove dup code, simplify

--- app/modules/feed/bart.js
+++ app/modules/feed/bart.js
@@ -9,60 +9,58 @@ angular.module('agencies')
         datetime.setTime(Date.parse(date + ' ' + time));
         return datetime;
     }
-
-    return {
-        getAllStops: function() {
-            var params = {};
-            params.cmd = 'stns';
+    function buildResource(resource, command, transform) {
+        return function(params) {
+            params.cmd = command;
             params.key = 'MW9S-E7SL-26DU-VV8V';
             var config = {
-                url: baseUrl + '/stn.aspx',
+                url: baseUrl + '/' + resource + '.aspx',
                 params: params,
                 transformResponse: function(response) {
                     var root = $(parser.parseFromString(response, 'text/xml'));
-                    var stations = $(root).find('station');
-                    var stops = _.map(stations, function(station) {
-                        var name = $(station).find('name').text();
-                        var agency = 'bart';
-                        var route = 'NA';
-                        var id = $(station).find('abbr').text();
-                        var lat = $(station).find('gtfs_latitude').text();
-                        var lon = $(station).find('gtfs_longitude').text();
-                        return Stop.createStop(name, agency, route, id, lat, lon);
-                    });
-                    return stops;
-                }
-            };
-            return $http(config);
-        },
-        getRidesForSegment: function(segment) {
-            var originStop = segment.getOriginNexus().getStops()[0];
-            var destinationStop = segment.getDestinationNexus().getStops()[0];
-            var config = {
-                url: 'http://api.bart.gov/api/sched.aspx',
-                params: {
-                    a: '4',
-                    cmd: 'depart',
-                    date: 'now',
-                    key: 'MW9S-E7SL-26DU-VV8V',
-                    orig: originStop.getStopId(),
-                    dest: destinationStop.getStopId()
-                },
-                transformResponse: function(response) {
-                    var root = $(parser.parseFromString(response, 'text/xml'));
-                    var trips = $(root).find('trip');
-                    var rides = _.map(trips, function(trip) {
-                        var agency = 'bart';
-                        var route = 'NA'; // need to get this from leg element
-                        var vehicle = 'NA'; // need to get this from leg element
-                        var startTime = convertToDate($(trip).attr('origTimeDate'), $(trip).attr('origTimeMin'));
-                        var endTime = convertToDate($(trip).attr('destTimeDate'), $(trip).attr('destTimeMin'));
-                        return Ride.createRide(agency, route, vehicle, startTime, endTime);
-                    });
-                    return rides;
+                    return transform(root);
                 }
             };
             return $http(config);
         }
     }
-}]);
+    function transformStationsToStops(root) {
+        var stations = $(root).find('station');
+        var stops = _.map(stations, function(station) {
+            var name = $(station).find('name').text();
+            var agency = 'bart';
+            var route = 'NA';
+            var id = $(station).find('abbr').text();
+            var lat = $(station).find('gtfs_latitude').text();
+            var lon = $(station).find('gtfs_longitude').text();
+            return Stop.createStop(name, agency, route, id, lat, lon);
+        });
+        return stops;
+    }
+    function transformScheduleToRides(root) {
+        var trips = $(root).find('trip');
+        var rides = _.map(trips, function(trip) {
+            var agency = 'bart';
+            var route = 'NA'; // need to get this from leg element
+            var vehicle = 'NA'; // need to get this from leg element
+            var startTime = convertToDate($(trip).attr('origTimeDate'), $(trip).attr('origTimeMin'));
+            var endTime = convertToDate($(trip).attr('destTimeDate'), $(trip).attr('destTimeMin'));
+            return Ride.createRide(agency, route, vehicle, startTime, endTime);
+        });
+        return rides;
+    }
+
+    return {
+        getAllStops: function() {
+            return buildResource('stn', 'stns', transformStationsToStops)({ })
+        },
+        getRidesForSegment: function(segment) {
+            var originStop = segment.getOriginNexus().getStops()[0];
+            var destinationStop = segment.getDestinationNexus().getStops()[0];
+            var args = { a: '4', date: 'now', orig: originStop.getStopId(), dest: destinationStop.getStopId() };
+            return buildResource('sched', 'depart', transformScheduleToRides)(args)
+        }
+     }
+    }
+
+    ]);
