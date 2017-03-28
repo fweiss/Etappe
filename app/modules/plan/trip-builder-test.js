bd39e35a10cde47ae4c6b0cf02e3c41f048ebcd4
Frank E Weiss <feweiss@gmail.com>
2017-03-27 20:02:45 -0700

    Move the old non-angular source out of the way

---
+++ app/modules/plan/trip-builder-test.js
@@ -0,0 +1,22 @@
+// start with an empty list of waypoints
+describe('trip builder', function() {
+    describe('initial', function() {
+        it('has empty waypoints', function() {});
+        it('has default agencies', function() {});
+        it('has default nexuses', function() {});
+        it('is incomplete', function() {});
+        it('can extend', function() {});
+    });
+    describe('add one waypoint', function() {
+        beforeEach(function() {});
+        it('has one waypoint', function() {});
+        it('is incomplete', function() {});
+        it('can extend', function() {});
+    });
+    describe('add two waypoints', function() {
+        beforeEach(function() {});
+        it('has two waypoints', function() {});
+        it('is complete', function() {});
+        it('can extend', function() {});
+    });
+});
