a6ca397f1d55fb7fedefd8f03d6b14bcfcba5d32
Frank E Weiss <feweiss@gmail.com>
2018-01-06 19:05:36 -0800

    Refactor: remove dead code

--- protractor/trip-storage.spec.js
+++ protractor/trip-storage.spec.js
@@ -9,20 +9,12 @@ describe('etappe', function() {
         })
         describe('list', function() {
             beforeEach(function() {
-                const xinitialTrip = '{ id: 1, tripName: "saved trip 1", '
-                    + 'origin: { waypointName: \'16th St & Mission St\', lat: 37.7651399, lon: -122.4196 },'
-                    + 'destination: { waypointName: \'Castro St\', lat: 37.7608499, lon: -122.43484 },'
-                    + 'waypoints: [ ] }'
-                const initialTrips = [ { id: 1, tripName: "saved trip 1",
+                 const initialTrips = [ { id: 1, tripName: "saved trip 1",
                     origin: { waypointName: '16th St & Mission St', lat: 37.7651399, lon: -122.4196 },
                     destination: { waypointName: 'Castro St', lat: 37.7608499, lon: -122.43484 },
                     waypoints: [ ] }]
-                // browser.executeScript('window.localStorage.setItem("savedTrips",' + JSON.stringify(initialTrips) +')')
-                const value = JSON.stringify(initialTrips).replace(/"/g, '\\"')
                 browser.executeScript('window.localStorage.setItem("savedTrips", arguments[0])', JSON.stringify(initialTrips));
-                // browser.executeScript('window.localStorage.setItem("savedTrips", "' + value + '")')
-                // browser.pause()
-                element(by.id('showSavedTrips')).click()
+                 element(by.id('showSavedTrips')).click()
             })
             it('without errors', function() {
                 expect(element(by.id('errors')).getText()).toEqual('');
